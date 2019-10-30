601e83ea5a6598962a0204d13a658993c715b521
Ray Klump <raymond.klump@gmail.com>
2019-10-29 19:14:12 -0500

    updated

--- GitDemo/src/MainApp.java
+++ GitDemo/src/MainApp.java
@@ -2,5 +2,6 @@
 public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("My computer is slow today.");
+		System.out.println("Eclipse should make this easier than it is.");
 	}
 }
